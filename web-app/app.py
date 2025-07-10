6ab91ca2a54f431f80dd17397c08e1ef2a58bcc7
Asim <jogiaa@gmail.com>
2025-07-10 01:15:13 -0400

    app with sample logger

--- web-app/app.py
+++ web-app/app.py
@@ -1,21 +1,42 @@
 from flask import Flask, render_template, request
-from poc_agno import DocumentationWorkflow
+
+from poc_agno import CombinedSummarizedDocumentationWorkflow
 
 # Initialize the Flask application
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return render_template('app.html')
+
 
 @app.route("/document", methods=["POST"])
 def document():
     requestJson = request.get_json()
     source = requestJson['src']
     destination = requestJson['dst']
-    workflow = DocumentationWorkflow()
+    workflow = CombinedSummarizedDocumentationWorkflow(logger=HtmlLogger())
     result = workflow.run(source_file_path=source, destination_file_path=destination)
-    return { "result" : result.content }
+    return {"result": result.content}
+
+
+class HtmlLogger:
+    def info(self, msg, *args, **kwargs) -> None:
+        print("[HTML]" + msg)
+
+    def error(self, msg: str) -> None:
+        print("[HTML]" + msg)
+
+    def warning(self, msg, *args, **kwargs) -> None:
+        print("[HTML]" + msg)
+
+    def critical(self, msg, *args, **kwargs) -> None:
+        print("[HTML]" + msg)
+
+    def debug(self, msg, *args, **kwargs) -> None:
+        print("[HTML]" + msg)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
